40c0675447c6d591eb9773acd185fbe476fceb10
aks <abdulkuddusimarovv@gmail.com>
2024-01-10 18:04:11 +0300

    homeWork_lesson1_aks

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        UchBurch T =new UchBurch();
+        Scanner sd =  new Scanner(System.in);
+        System.out.println("enter sides of triangle : ");
+        T.a=sd.nextDouble();
+        T.b=sd.nextDouble();
+        T.c=sd.nextDouble();
+
+        T.area(T.a,T.b,T.c);
+    }
+}
---
+++ src/UchBurch.java
@@ -0,0 +1,15 @@
+public class UchBurch {
+    double a,b,c;
+    public  void  area(double a,double b, double c){
+        if (a + b > c && a + c > b && b + c > a) {
+            double p = (a + b + c) / 2; // полупериметр
+            double area = Math.sqrt(p * (p - a) * (p - b) * (p - c)); // формула Герона
+
+            System.out.println(" ######## area of triagle : "+area);
+        } else {
+            System.out.println("uncorrected sides of triangle!!! try again ");
+
+        }
+
+    }
+}
