b2b5b0c79673680dbe0f3767baedb8e033c67cd5
aks <abdulkuddusimarovv@gmail.com>
2024-01-10 18:41:03 +0300

    homeWork_encapsulation_aks

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+//
+//        UchBurch T = new UchBurch();
+//        Scanner sd = new Scanner(System.in);
+//        System.out.println("enter sides of triangle : ");
+//        T.a = sd.nextDouble();
+//        T.b = sd.nextDouble();
+//        T.c = sd.nextDouble();
+//        T.area(T.a, T.b, T.c);
+        Cat cat1 = new Cat();
+        cat1.setName("tom");
+        cat1.setStock("sera");
+        Fish fish1 = new Fish();
+        fish1.setColor("red");
+        fish1.setType("fugo");
 
-        UchBurch T = new UchBurch();
-        Scanner sd = new Scanner(System.in);
-        System.out.println("enter sides of triangle : ");
-        T.a = sd.nextDouble();
-        T.b = sd.nextDouble();
-        T.c = sd.nextDouble();
-        T.area(T.a, T.b, T.c);
+        System.out.println("Мышык "+cat1.getName()+" "+fish1.getColor()+"тустогу "+fish1.getType()+"ну жегиси келди ))))");
     }
 }
